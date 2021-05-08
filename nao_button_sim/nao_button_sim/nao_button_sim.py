5ecf086d33ba9f811acb75e146c50d67e2be066d
Kenji Brameld <kenjibrameld@gmail.com>
2021-05-08 21:42:03 +0900

    add nao_button_test as a apython package

---
+++ nao_button_sim/nao_button_sim/nao_button_sim.py
@@ -0,0 +1,92 @@
+import rclpy
+from rclpy.node import Node
+from pynput import keyboard
+
+from nao_interfaces.msg import Buttons
+
+
+chest = False
+l_foot_bumper_left = False
+l_foot_bumper_right = False
+r_foot_bumper_left = False
+r_foot_bumper_right = False
+
+
+def on_press(key):
+    try:
+        if key.char == 'g':
+            global chest
+            chest = True
+        elif key.char == 'c':
+            global l_foot_bumper_left
+            l_foot_bumper_left = True
+        elif key.char == 'v':
+            global l_foot_bumper_right
+            l_foot_bumper_right = True
+        elif key.char == 'b':
+            global r_foot_bumper_left
+            r_foot_bumper_left = True
+        elif key.char == 'n':
+            global r_foot_bumper_right
+            r_foot_bumper_right = True
+    except AttributeError:
+        pass
+
+
+def on_release(key):
+    try:
+        if key.char == 'g':
+            global chest
+            chest = False
+        elif key.char == 'c':
+            global l_foot_bumper_left
+            l_foot_bumper_left = False
+        elif key.char == 'v':
+            global l_foot_bumper_right
+            l_foot_bumper_right = False
+        elif key.char == 'b':
+            global r_foot_bumper_left
+            r_foot_bumper_left = False
+        elif key.char == 'n':
+            global r_foot_bumper_right
+            r_foot_bumper_right = False
+    except AttributeError:
+        pass
+
+
+class ButtonsPublisher(Node):
+
+    def __init__(self):
+        super().__init__('buttons_publisher')
+        self.publisher_ = self.create_publisher(Buttons, 'buttons', 10)
+        timer_period = 0.02  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = Buttons()
+        msg.chest = chest
+        msg.l_foot_bumper_left = l_foot_bumper_left
+        msg.l_foot_bumper_right = l_foot_bumper_right
+        msg.r_foot_bumper_left = r_foot_bumper_left
+        msg.r_foot_bumper_right = r_foot_bumper_right
+        self.publisher_.publish(msg)
+
+
+def main(args=None):
+    # listen in a non-blocking fashion:
+    listener = keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release)
+    listener.start()
+
+    rclpy.init(args=args)
+    buttons_publisher = ButtonsPublisher()
+    rclpy.spin(buttons_publisher)
+
+    # Destroy the node explicitly
+    buttons_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ nao_button_sim/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'nao_button_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ijnek',
+    maintainer_email='kenjibrameld@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'nao_button_sim = nao_button_sim.nao_button_sim:main'
+        ],
+    },
+)
