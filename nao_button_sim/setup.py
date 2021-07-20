bba30d41af0b17c5643b8bb6819b3bf6b18da738
Kenji Brameld <kenjibrameld@gmail.com>
2021-07-20 14:53:53 +0900

    add package description for nao_button_sim

--- nao_button_sim/setup.py
+++ nao_button_sim/setup.py
@@ -15,8 +15,8 @@ setup(
     zip_safe=True,
     maintainer='ijnek',
     maintainer_email='kenjibrameld@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Allows simulating button presses through command line interface',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
